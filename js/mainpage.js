097751e86ef40324434635f78f028dbd76172dea
PongXD <brendanyhy@gmail.com>
2017-04-09 21:15:49 +0800

    Added a popup for a new registration
    use these values to activate the pop up
    username = newuser
    password = password123
    email = any string (cannot be blank)

--- js/mainpage.js
+++ js/mainpage.js
@@ -1,14 +1,48 @@
 
-/* Initialise login pop up */
+/* Initialise pop ups */
 $(document).ready(function () {
     $('#openlogin').popup({
-        //type: 'tooltip',
         offsettop: 100,
         offsetleft: -200,
         opacity: 0.3,
         transition: 'all 0.5s',
         outline: true
     });
+});
+
+$(document).ready(function () {
+    $('#openNewUser').popup({
+        offsettop: 100,
+        offsetleft: -200,
+        opacity: 0.3,
+        transition: 'all 0.5s',
+        outline: true
+    });
+});
+
+/* Read form values for mainpage-loggedin */
+
+function getParameterByName(name, url) {
+    if (!url) {
+      url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+var username = getParameterByName('username');
+var password = getParameterByName('password');
+var email = getParameterByName('email');
+
+$(document).ready(function () {
+    if(username == "newuser" && password == "password123" && email != "") {
+        document.getElementById('username-display').innerHTML = username;
+        $('#openNewUser').popup('show');
+    }
 });
 
 /* Back top
